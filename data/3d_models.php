8e9ecb8d4289321c540bd9fa651144d37d94f46f
egn-thomas <thomas.eugene.62250@gmail.com>
2026-02-16 16:57:46 +0100

    api: récupération des images depuis cloudflare

--- data/3d_models.php
+++ data/3d_models.php
@@ -3,25 +3,25 @@ $models_3d = [
     [
         'title' => 'Chambre Isométrique',
         'description' => 'Modèle 3D d\'une chambre en vue isométrique. Le modèle inclut une sumulation de tissus pour le lit ce qui m\'a permis d\'apprehender le fonctionnement de la simulation de tissus et drapés dans Blender. C\'est également mon premier projet de modélisation 3D réalisé en autonomie.',
-        'image' => 'https://2d22915c5dcb387fa89b6ebd7283c3b0.r2.cloudflarestorage.com/portfolio/images/3d/chambreIsometrique.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/3d/chambreIsometrique.png',
         'date' => '2025-05'
     ],
     [
         'title' => 'Fiole',
         'description' => 'Modélisation d\'une fiole en verre. Inspirée d\'un tutoriel de Bertrand . Tech, un artiste 3D',
-        'image' => 'https://2d22915c5dcb387fa89b6ebd7283c3b0.r2.cloudflarestorage.com/portfolio/images/3d/tuto1Fiole.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/3d/tuto1Fiole.png',
         'date' => '2025-03'
     ],
     [
         'title' => 'Tourelle de Château',
         'description' => 'Design d\'une tourelle médiévale. Inspirée d\'un tutoriel de Bertrand . Tech, un artiste 3D',
-        'image' => 'https://2d22915c5dcb387fa89b6ebd7283c3b0.r2.cloudflarestorage.com/portfolio/images/3d/tuto2Tour.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/3d/tuto2Tour.png',
         'date' => '2025-04'
     ],
     [
         'title' => 'Citrouille d\'Halloween',
         'description' => 'Design d\'une citrouille sculptée pour Halloween. Inspirée d\'un tutoriel de Bertrand . Tech, un artiste 3D',
-        'image' => 'https://2d22915c5dcb387fa89b6ebd7283c3b0.r2.cloudflarestorage.com/portfolio/images/3d/tuto3Citrouille.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/3d/tuto3Citrouille.png',
         'date' => '2025-09'
     ]
 ];
--- data/illustrations.php
+++ data/illustrations.php
@@ -3,25 +3,25 @@ $illustrations = [
     [
         'title' => 'Painting Digital "Tete de mort géante"',
         'description' => 'Illustration numérique réalisée sur Photoshop. Pour représenter l\'ambiance sombre et nocturne de ce painting, j\'ai du faire un travail assez conséquend sur les ombres et les lumieres. Notamment en utilisant des calques de réglages pour ajuster les contrastes et les niveaux de luminosité.',
-        'image' => 'assets/images/illustrations/teteDeMortGeante.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/illustrations/teteDeMortGeante.png',
         'date' => '2025-02'
     ],
     [
         'title' => 'Painting Digital "Grenouille Mécanique"',
         'description' => 'Concept art d\'une grenouille mécanique réalisé sur Photoshop. J\'ai utilisé des techniques de peinture numérique pour créer les textures métalliques et donner vie à cette créature imaginaire. Le travail sur les reflets et les ombres a été crucial pour rendre l\'effet réaliste.',
-        'image' => 'assets/images/illustrations/grenouilleMecanique.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/illustrations/grenouilleMecanique.png',
         'date' => '2025-03'
     ],
     [
         'title' => 'Painting Digital "Forêt Enchantée"',
         'description' => 'Illustration numérique d\'une forêt enchantée réalisée sur Photoshop. J\'ai mis l\'accent sur les jeux de lumière pour guider le regard et donner une impression de calme dans ce paysage.',
-        'image' => 'assets/images/illustrations/foret.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/illustrations/foret.png',
         'date' => '2025-06'
     ],
     [
         'title' => 'Painting Digital "Le Scaphandrier Perdu"',
         'description' => 'Concept art d\'un scaphandrier perdu dans les profondeurs de l\'océan, réalisé sur Photoshop. J\'ai utilisé des techniques de peinture numérique pour créer une atmosphère mystérieuse et immersive, en jouant avec les nuances de bleu et les effets de lumière sous-marine.',
-        'image' => 'assets/images/illustrations/scaphandre.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/illustrations/scaphandre.png',
         'date' => '2024-09'
     ]
 ];
