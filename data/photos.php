c979bf01b85808ad70e5c424f10292f9ea862ac7
egn-thomas <thomas.eugene.62250@gmail.com>
2026-02-16 16:41:57 +0100

    api: récupération des images depuis cloudflare

--- data/photos.php
+++ data/photos.php
@@ -3,19 +3,19 @@ $photos = [
     [
         'title' => 'Portrait en Noir et Blanc',
         'description' => 'Portrait studio en lumière naturelle',
-        'image' => 'assets/images/photos/portrait1.jpg',
+        'image' => 'https://2d22915c5dcb387fa89b6ebd7283c3b0.r2.cloudflarestorage.com/portfolio/images/photos/portrait1.jpg',
         'date' => '2024-01'
     ],
     [
         'title' => 'Paysage Urbain',
         'description' => 'Vue de Paris depuis Montmartre',
-        'image' => 'assets/images/photos/paysage1.jpg',
+        'image' => 'https://2d22915c5dcb387fa89b6ebd7283c3b0.r2.cloudflarestorage.com/portfolio/images/photos/paysage1.jpg',
         'date' => '2024-02'
     ],
     [
         'title' => 'Nature Morte',
         'description' => 'Composition avec objets anciens',
-        'image' => 'assets/images/photos/nature1.jpg',
+        'image' => 'https://2d22915c5dcb387fa89b6ebd7283c3b0.r2.cloudflarestorage.com/portfolio/images/photos/nature1.jpg',
         'date' => '2024-03'
     ]
 ];
