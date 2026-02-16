019d7435d9d0ec9aef1378d5f33dca77bf84eda0
egn-thomas <thomas.eugene.62250@gmail.com>
2026-02-16 17:02:37 +0100

    api: récupération des images depuis cloudflare

--- data/dev_projects.php
+++ data/dev_projects.php
@@ -5,9 +5,9 @@ $dev_projects = [
         'description' => 'Jeu de plateforme 2D de type RPG développé en C++',
         'long_description' => 'Un heu de plateforme 2D inspiré des classiques du RPG, développé en C++ avec la bibliothèque SFML. Ce jeu dispose de plusieurs niveaux, chacun généré proceduralemenet, avec des ennemis variés et des objets à collecter. Le joueur peut améliorer ses compétences et son équipement au fur et à mesure de sa progression dans le jeu.',
         'gallery' => [
-            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/dev_projects/app1.png',
-            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/dev_projects/app2.png',
-            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/dev_projects/app3.png'
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/app1.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/app2.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/app3.png'
         ],
         'technologies' => ['C++', 'SFML', 'Git'],
         'github' => 'https://github.com/votre-repo',
@@ -18,11 +18,11 @@ $dev_projects = [
         'title' => 'Plateforme web de gestion de classes scolaires',
         'description' => 'Aide à la gestion des classses dans un collège développé avec python et flask',
         'long_description' => 'Cette application web permet aux enseignants de gérer facilement leurs classes, de suivre les progrès des élèves et de communiquer avec les parents. Développée en Python avec le framework Flask, l\'application offre une interface utilisateur intuitive et des fonctionnalités robustes telles que la génération de classes en fonction des choix des élèves en langues ou options par exemple.',
-        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/dev_projects/game1.png',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/game1.png',
         'gallery' => [
-            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/dev_projects/game1.png',
-            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/dev_projects/game2.png',
-            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/dev_projects/game3.png'
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/game1.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/game2.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/game3.png'
         ],
         'technologies' => ['Python', 'Flask', 'SQLite'],
         'github' => 'https://github.com/votre-repo',
--- data/photos.php
+++ data/photos.php
@@ -3,19 +3,19 @@ $photos = [
     [
         'title' => 'Portrait en Noir et Blanc',
         'description' => 'Portrait studio en lumière naturelle',
-        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/photos/portrait1.jpg',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/photos/portrait1.jpg',
         'date' => '2024-01'
     ],
     [
         'title' => 'Paysage Urbain',
         'description' => 'Vue de Paris depuis Montmartre',
-        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/photos/paysage1.jpg',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/photos/paysage1.jpg',
         'date' => '2024-02'
     ],
     [
         'title' => 'Nature Morte',
         'description' => 'Composition avec objets anciens',
-        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/portfolio/images/photos/nature1.jpg',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/photos/nature1.jpg',
         'date' => '2024-03'
     ]
 ];
