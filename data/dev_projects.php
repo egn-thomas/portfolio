29d42e169098e6555f1660b89c87984366c78361
egn-thomas <thomas.eugene.62250@gmail.com>
2026-02-17 17:54:06 +0100

    update: developpement section

--- data/dev_projects.php
+++ data/dev_projects.php
@@ -3,7 +3,10 @@ $dev_projects = [
     [
         'title' => 'Jeu de plateforme en C++',
         'description' => 'Jeu de plateforme 2D de type RPG développé en C++',
-        'long_description' => 'Un jeu de plateforme 2D inspiré des classiques du RPG, développé en C++ avec la bibliothèque SFML. Ce jeu dispose de plusieurs niveaux, chacun généré proceduralemenet, avec des ennemis variés et des objets à collecter. Le joueur peut améliorer ses compétences et son équipement au fur et à mesure de sa progression dans le jeu.',
+        'long_description' => 'Un jeu de plateforme 2D inspiré des classiques du RPG. <br><br>
+                               Le jeu propose une aventure de survie à travers des niveaux générés procéduralement à la manière d\'un labyrinthe (voir image 3). Le joueur incarne un personnage qui doit explorer ces niveaux, collecter des ressources (voir image 4), combattre des ennemis et résoudre des énigmes pour progresser. <br><br>
+                               Le développement de ce projet m\'a permis d\'approfondir mes compétences en C++ en déployant une architecture de code SOLID en parfaite autonomie. Cela m\'a permis de maintenir un code propre et évolutif. J\'ai également utilisé la bibliothèque SFML pour gérer les aspects graphiques du jeu. <br><br>
+                               Au delà de l\'aspect technique, ce projet a été pour moi un moyen de mettre en pratique mes connaissances en illustration et animation car j\'ai tenu a créer moi même tous les assets graphiques du jeu, ce qui m\'a permis de développer une cohérence visuelle forte et de donner vie à mon univers de jeu.',
         'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/gameOfThomas/gameOfThomas-Presentation.png',
         'gallery' => [
             'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/gameOfThomas/gameOfThomas-Presentation.png',
@@ -18,9 +21,9 @@ $dev_projects = [
     ],
     [
         'title' => 'Logiciel de traitement d\'images spatiales',
-        'description' => 'Outil de réduction des images spatiales développé en python a l\'occasion d\'une SAÉ organisée par le département informatique de l\'IUT de calais',
+        'description' => 'Outil de réduction des images spatiales développé en python par équipe de 4 à l\'occasion d\'une SAÉ organisée par le département informatique de l\'IUT de calais',
         'long_description' => 'Ce projet a été réalisé dans le cadre d\'une SAÉ organisée par le département informatique de l\'IUT de Calais. Il s\'agit d\'un logiciel de traitement d\images spatiales qui a pour vocation de réduire la taille des étoiles sur des images astronomiques afin de faciliter leur analyse. <br><br>
-        Le logiciel utilise des technique de traitement d\'images tel que la dilatation et l\'érosion pour réduire la taille des étoiles tout en préservant les détails importants de l\'image. Il offre une interface utilisateur simple et intuitive, permettant aux utilisateurs de charger leurs images, d\'appliquer le traitement et de visualiser les résultats facilement. <br><br>
+                               Le logiciel utilise des technique de traitement d\'images tel que la dilatation et l\'érosion pour réduire la taille des étoiles tout en préservant les détails importants de l\'image. Il offre une interface utilisateur simple et intuitive, permettant aux utilisateurs de charger leurs images, d\'appliquer le traitement et de visualiser les résultats facilement. <br><br>
                                Ce projet a également été pour moi l\'occasion d\'aborder le développement d\une intelligence artificielle via machine learning, qui a été utilisée pour améliorer la précision du traitement en apprenant à reconnaître les étoiles et à ajuster les paramètres de réduction en conséquence.',
         'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-GoodSettings.png',
         'gallery' => [
@@ -33,20 +36,5 @@ $dev_projects = [
             'date' => '2026-01',
             'role' => 'Développeur'
     ]
-    // [
-    //     'title' => 'Plateforme web de gestion de classes scolaires',
-    //     'description' => 'Aide à la gestion des classses dans un collège développé en php avec persistance de données Mon',
-    //     'long_description' => 'Cette application web permet aux enseignants de gérer facilement leurs classes, de suivre les progrès des élèves et de communiquer avec les parents. Développée en Python avec le framework Flask, l\'application offre une interface utilisateur intuitive et des fonctionnalités robustes telles que la génération de classes en fonction des choix des élèves en langues ou options par exemple.',
-    //     'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/game1.png',
-    //     'gallery' => [
-    //         'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/game1.png',
-    //         'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/game2.png',
-    //         'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/dev_projects/game3.png'
-    //     ],
-    //     'technologies' => ['Python', 'Flask'],
-    //     'github' => 'https://github.com/votre-repo',
-    //     'date' => '2025-08',
-    //     'role' => 'Développeur Web'
-    // ]
 ];
 ?>
