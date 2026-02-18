47f3d64419ce42bc7c050f396c34a2d45eb62ac0
egn-thomas <thomas.eugene.62250@gmail.com>
2026-02-18 15:59:09 +0100

    add: content to illustration

--- data/illustrations.php
+++ data/illustrations.php
@@ -1,10 +1,40 @@
 <?php
 $illustrations = [
     [
-        'title' => 'Painting Digital "Tete de mort géante"',
-        'description' => 'Illustration numérique réalisée sur Photoshop. Pour représenter l\'ambiance sombre et nocturne de ce painting, j\'ai du faire un travail assez conséquend sur les ombres et les lumieres. Notamment en utilisant des calques de réglages pour ajuster les contrastes et les niveaux de luminosité.',
-        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/teteDeMortGeante.png',
-        'date' => '2025-02'
+        'title' => 'Painting Digital "Forêt Enchantée"',
+        'description' => 'Illustration numérique d\'une forêt enchantée réalisée sur Photoshop. J\'ai mis l\'accent sur les jeux de lumière pour guider le regard et donner une impression de calme dans ce paysage.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/foret.png',
+        'date' => '2025-12'
+    ],
+    [
+        'title' => 'Painting Digital "Blizzard"',
+        'description' => 'Concept art d\'un aventurier affrontant une tempête de neige, réalisé sur procreate. J\'ai utilisé la profondeur de champ pour créer une atmosphère immersive et donner une impression de froid glacial, en jouant avec les nuances de blanc et les effets de lumière dans la tempête.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/blizzard.png',
+        'date' => '2025-10'
+    ],
+    [
+        'title' => 'Painting Digital "Le Scaphandrier Perdu"',
+        'description' => 'Concept art d\'un scaphandrier perdu dans les profondeurs de l\'océan, réalisé sur Photoshop. J\'ai utilisé des techniques de peinture numérique pour créer une atmosphère mystérieuse et immersive, en jouant avec les nuances de bleu et les effets de lumière sous-marine.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/scaphandre.png',
+        'date' => '2024-09'
+    ],
+    [
+        'title' => 'Painting Digital "L\'Éveil de la Bûche"',
+        'description' => 'Mise en scène d\'un petit être caché derrière une bûche, chassant des papillons. J\'ai voulu représenter une atmosphère paisible et magique, en jouant avec les couleurs douces et les effets de lumière pour créer une ambiance féerique.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/bucheEtPapillons.png',
+        'date' => '2025-07'
+    ],
+    [
+        'title' => 'Painting Digital "Magicien vert"',
+        'description' => 'Character design d\'un guerrier à l\'allure déterminé. Les effets de lumière qui s\'échappent de ses mains et ses yeux laissent entrevoir qu\'il est doté de pouvoirs magiques.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/magicienVert.png',
+        'date' => '2025-06'
+    ],
+    [
+        'title' => 'Painting Digital "Pics Montagneux"',
+        'description' => 'Sous le soleil couchant, une créature mysterieuse se tient devant une étendue de montagnes abruptes.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/picsMontagneux.png',
+        'date' => '2025-06'
     ],
     [
         'title' => 'Painting Digital "Grenouille Mécanique"',
@@ -13,16 +43,28 @@ $illustrations = [
         'date' => '2025-03'
     ],
     [
-        'title' => 'Painting Digital "Forêt Enchantée"',
-        'description' => 'Illustration numérique d\'une forêt enchantée réalisée sur Photoshop. J\'ai mis l\'accent sur les jeux de lumière pour guider le regard et donner une impression de calme dans ce paysage.',
-        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/foret.png',
-        'date' => '2025-06'
+        'title' => 'Painting Digital "Tete de mort géante"',
+        'description' => 'Illustration numérique réalisée sur Photoshop. Pour représenter l\'ambiance sombre et nocturne de ce painting, j\'ai du faire un travail assez conséquend sur les ombres et les lumieres. Notamment en utilisant des calques de réglages pour ajuster les contrastes et les niveaux de luminosité.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/teteDeMortGeante.png',
+        'date' => '2025-05'
     ],
     [
-        'title' => 'Painting Digital "Le Scaphandrier Perdu"',
-        'description' => 'Concept art d\'un scaphandrier perdu dans les profondeurs de l\'océan, réalisé sur Photoshop. J\'ai utilisé des techniques de peinture numérique pour créer une atmosphère mystérieuse et immersive, en jouant avec les nuances de bleu et les effets de lumière sous-marine.',
-        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/scaphandre.png',
-        'date' => '2024-09'
-    ]
+        'title' => 'Painting Digital "Scorpion"',
+        'description' => 'Inspiré du personnage de marvel du même nom, j\'ai utilisé les code du comic book pour m\'approprier ce personnage et le mettre en scène sur le toit d\'un gratte ciel.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/scorpion.png',
+        'date' => '2024-11'
+    ],
+    [
+        'title' => 'Painting Digital "Cité Volante"',
+        'description' => 'Digital painting d\'une cité volante en ruine de laquelle surgit un énorme dragon écarlate.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/citeVolante.png',
+        'date' => '2024-05'
+    ],
+    [
+        'title' => 'Painting Digital "Samurail"',
+        'description' => 'Character design d\'un samurail réalisé à mes debuts sur procreate. Il semble disparaitre dans un épais nuage de fumée multicolore tout en rengainant son sabre.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/illustrations/samurai.png',
+        'date' => '2024-02'
+    ],    
 ];
 ?>
