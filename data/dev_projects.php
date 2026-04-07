3e5a48ede90fdc6549467f615b157b2bbba49fb1
egn-thomas <thomas.eugene.62250@gmail.com>
2026-04-07 10:43:40 +0200

    Update dev_projects.php

--- data/dev_projects.php
+++ data/dev_projects.php
@@ -30,11 +30,73 @@ $dev_projects = [
             'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-GoodSettings.png',
             'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-BadSettings.png',
             'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-IAmode.png'
-            ],
-            'technologies' => ['Python', 'PyQt'],
-            'github' => 'https://github.com/egn-thomas/SAE_astrophoto.git',
-            'date' => '2026-01',
-            'role' => 'Développeur'
-    ]
+        ],
+        'technologies' => ['Python', 'PyQt'],
+        'github' => 'https://github.com/egn-thomas/SAE_astrophoto.git',
+        'date' => '2026-01',
+        'role' => 'Développeur'
+    ],
+    [
+        'title' => 'ALTO - Le réseau social IRL',
+        'description' => 'Projet de développement d\'un réseau social IRL en équipe de 2, réalisé dans le cadre du cours de développement d\'application web',
+        'long_description' => 'ALTO est une plateforme de réseau social IRL (In Real Life) qui utilise un système de pairing via QR code pour connecter des utilisateurs dans la vie réelle. L\'objectif de ce projet étant de créer une application de réseau social qui favorise la sécurité des jeunes utilisateurs en limitant les interactions en ligne et en encourageant les rencontres en personne. <br><br>
+                               Ce projet a été réalisé avec Futter, un framework de développement d\'applications mobiles, ce qui m\'a permis d\'apprendre à développer pour les plateformes iOS et Android. L\'application offre une interface utilisateur intuitive et conviviale, permettant de se connecter avec d\'autres utilisateurs via QR code et de partager des moments de leur vie réelle en toute sécurité. <br><br>
+                               Ce projet m\'a permis d\'apprendre à utiliser le framework Flutter pour le développement d\'applications mobiles, ainsi que de renforcer mes compétences en développement d\'applications web et en gestion de projet en équipe.',
+
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-GoodSettings.png',
+        'gallery' => [
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-GoodSettings.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-BadSettings.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-IAmode.png'
+        ],
+        'technologies' => ['Flutter', 'Dart'],
+        'github' => 'https://github.com/egn-thomas/alto.git',
+        'date' => '2026-03',
+        'role' => 'Développeur, UI/UX Designer'
+    ],
+    [
+        'title' => 'Pokemon TCG - Application de collection de cartes à jouer Pokemon',
+        'description' => 'Projet de développement web en nodeJS d\'une application de cartes pokemon à collectionner.',
+        'long_description' => 'Pokemon TCG est une application de gestion de collection de cartes à jouer Pokemon. L\'application permet aux utilisateurs de créer et de gérer leur collection de cartes, d\'ajouter des cartes à leur collection, de faire des combats avec d\'autres utilisateurs. <br><br>
+                               Ce projet a été réalisé avec NodeJS pour le backend et VueJS pour le frontend, ce qui m\'a permis d\'apprendre à développer une application web complète en utilisant ces technologies qui sont très utilisées dans de vraies applications professionnelles. Le cadre de travail mis en place durant ce TP était opptimisé pour nous rapprocher le plus possible du travail en entreprise.<br><br>
+                               Ce projet m\'a permis d\'apprendre à utiliser NodeJS et Vue pour le développement web. Il m\'a également permis d\'aborder un environnement de travail professionnel, notamment via l\'utilisation de Git',
+
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-GoodSettings.png',
+        'gallery' => [
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-GoodSettings.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-BadSettings.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/astrophoto/astrophoto-IAmode.png'
+        ],
+        'technologies' => ['NodeJS', 'VueJS', 'Git'],
+        'github' => 'https://github.com/celien-svg/alto.git',
+        'date' => '2026-03',
+        'role' => 'Développeur'
+    ],
+    [
+        'title' => 'Chess.preview - Application web, api et base de données pour la prévisualisation de parties d\'échecs',
+        'description' => 'Projet de développement web en php et nodeJS d\'une application de prévisualisation et de prévision de parties d\'échecs.',
+        'long_description' => 'Chess.preview est une application web qui permet aux utilisateurs de prévisualiser des parties d\'échecs mais aussi de simuler des affrontements en tentant de prédire l\'issue du match. <br><br>
+                               Lors de la réalisation de ce projet dans le cadre d\'une SAÉ, nous avons du mettre en place une base de données relationnelle pour stocker les données des parties d\'échecs, ainsi qu\'une API RESTful pour permettre à l\'application web de communiquer avec la base de données. Le frontend a été développé en PHP, tandis que le backend a été réalisé en NodeJS. <br><br>
+                               Ce projet m\'a permis d\'apprendre à concevoir et à implémenter une base de données relationnelle, ainsi qu\'à développer une API REST pour permettre la communication entre le frontend et le backend. J\'ai également renforcé mes compétences en développement web en utilisant PHP et NodeJS pour créer une application complète.',
+        'image' => 'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-12.png',
+        'gallery' => [
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-01.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-02.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-03.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-04.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-05.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-06.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-07.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-08.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-09.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-10.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-11.png',
+            'https://pub-d6e16cabe530450d941567e9209c59fb.r2.dev/images/developpement/ChessPreview/Chess-preview-12.png',
+        ],
+        'technologies' => ['PHP', 'NodeJS', 'Git', 'PostgreSQL'],
+        'github' => 'https://github.com/IUT-Calais/sae-but-informatique-2-projet-web-api-dockerise-groupe-3',
+        'date' => '2026-03',
+        'role' => 'Développeur, Database Designer, API Developer, UI/UX Designer'
+    ],
 ];
 ?>
